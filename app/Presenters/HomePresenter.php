16f8636ec91a43d43154f0af8301c72981ef4e21
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-09-14 12:14:03 +0200

    connect & render  database

--- app/Presenters/HomePresenter.php
+++ app/Presenters/HomePresenter.php
@@ -5,8 +5,24 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
+
 
 
 final class HomePresenter extends Nette\Application\UI\Presenter
 {
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+  public function renderDefault(): void
+  {
+    $this->template->posts = $this->database
+      ->table('posts')
+      ->order('created_at DESC')
+      ->limit(5);
+  }
+  
 }
+
