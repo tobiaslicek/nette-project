6d8266f66bccd2e46bf789b2c837f4442888605f
Tobiáš Licek <tobiaslicek@seznam.cz>
2023-09-15 08:17:57 +0200

    add comment history

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -22,6 +22,7 @@ final class PostPresenter extends Nette\Application\UI\Presenter
     }
 
     $this->template->post = $post;
+	  $this->template->comments = $post->related('comments')->order('created_at');
   }
 
   protected function createComponentCommentForm(): Form
@@ -38,6 +39,23 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 
     $form->addSubmit('send', 'Publikovat komentář');
 
+    $form->onSuccess[] = $this->commentFormSucceeded(...);
+
     return $form;
+  }
+
+  private function commentFormSucceeded(\stdClass $data): void
+  {
+    $postId = $this->getParameter('postId');
+
+    $this->database->table('comments')->insert([
+      'post_id' => $postId,
+      'name' => $data->name,
+      'email' => $data->email,
+      'content' => $data->content,
+    ]);
+
+    $this->flashMessage('Děkuji za komentář', 'success');
+    $this->redirect('this');
   }
 }
